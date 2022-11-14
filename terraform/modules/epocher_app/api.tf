6ee6b272413a6dd6a85c1930939d349ba479a301
Alfonso Grana Gorlero <agrana@wcc-group.com>
2022-11-14 22:44:15 +0100

    Better stage name

--- terraform/modules/epocher_app/api.tf
+++ terraform/modules/epocher_app/api.tf
@@ -54,7 +54,7 @@ resource "aws_api_gateway_deployment" "example" {
 resource "aws_api_gateway_stage" "example" {
   deployment_id        = aws_api_gateway_deployment.example.id
   rest_api_id          = aws_api_gateway_rest_api.example.id
-  stage_name           = "example"
+  stage_name           = "${var.environment}-${var.application_name}"
   xray_tracing_enabled = true
 }
 
